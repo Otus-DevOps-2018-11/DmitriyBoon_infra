fcfcd17fe396bb77482c72d7f8476924846ced1a
Dmitriy Boon <dboon@mail.ru>
2019-01-12 17:35:23 +0300

    testapp check travis 03

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
+sudo git clone -b monolith https://github.com/express42/reddit.git
+sudo cd reddit && bundle install
+sudo puma -d
