2045c178c517d8202d6228ee8d850f496e7d19c9
Dmitriy Boon <dboon@mail.ru>
2019-01-28 22:05:14 +0300

    terraform-2 fi3

--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -4,3 +4,18 @@
 # output "lb_external_ip" {
 #   value = "${google_compute_forwarding_rule.default.ip_address}"
 # }
+output "apps_external_ip" {
+  value = "${module.app.apps_external_ip}"
+}
+
+output "apps_local_ip" {
+  value = "${module.app.apps_local_ip}"
+}
+
+output "db_external_ip" {
+  value = "${module.db.db_external_ip}"
+}
+
+output "db_local_ip" {
+  value = "${module.db.db_local_ip}"
+}
