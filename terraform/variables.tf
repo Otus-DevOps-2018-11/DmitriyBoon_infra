f4bda2053df6789ba5188186bc65c56b2d453315
Dmitriy Boon <dboon@mail.ru>
2019-01-31 17:48:11 +0300

    repair conflict

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -46,17 +46,3 @@ variable region {
 #   default     = ["0.0.0.0/0"]
 # }
 
-<<<<<<< HEAD
-=======
-variable "node_count" {
-  default = "1"
- }
-variable app_disk_image {
-  description = "Disk image for reddit app"
-  default = "reddit-base-1548510468"
-  }
-variable db_disk_image {
-  description = "Disk image for reddit db"
-  default = "reddit-base-1548510831"
-}
->>>>>>> 97482f171291508cc05def9f3f52496d04649bc9
